d7e88e9cbb5702afc2c324fae2b766079c1e2204
jleeothon <jleeothon@gmail.com>
2015-11-22 23:43:46 +0100

    Added a couple of tests plus ROOT_O
    And corrected the title in the readme

--- corto/corto.py
+++ corto/corto.py
@@ -1,12 +1,31 @@
 import ctypes
 import os
 
+
 CORTO_HOME = os.environ.get('CORTO_HOME', '/usr/local/')
 
+
 libcorto = ctypes.CDLL("%s/lib/corto/0.2/libraries/libcorto.so" % (CORTO_HOME))
+
+
+ROOT_O = ctypes.c_void_p.in_dll(libcorto, "root_o")
+
 
 def start():
     libcorto.corto_start()
 
+
 def stop():
     libcorto.corto_stop()
+
+
+def resolve(scope, name):
+    return libcorto.corto_resolve(scope, name)
+
+
+def parentof(o):
+    return libcorto.corto_parentof(o)
+
+
+def nameof(o):
+    return libcorto.corto_nameof(o)
